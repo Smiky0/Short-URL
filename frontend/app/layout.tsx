dd9fbafd3dde12a2c1105f2bd8181e8f09a67b85
Soumik <smik1172@gmail.com>
2025-08-30 02:31:21 +0530

    changed font style

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,34 +1,36 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Delius_Swash_Caps, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const deliusSans = Delius_Swash_Caps({
+    variable: "--font-delius-sans",
+    preload: true,
+    weight: "400",
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Make your URL short!",
-  description: "Glow-up for those long, annoying, meaningless URLs",
+    title: "Make your URL short!",
+    description: "Glow-up for those long, annoying, meaningless URLs",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${deliusSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
